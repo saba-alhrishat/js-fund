0c1a664bca9e0d7b9a5dd025b262dadf2407937f
Saba Al hrishat <saba.alhrishat2002@gmail.com>
2024-12-30 11:09:58 +0300

    test

---
+++ b1.js
@@ -0,0 +1,58 @@
+let cash = 1000;
+let current =500;
+console.log(cash / current);
+
+let revenuse = 1000;
+let expenses =500;
+document.writeln(cash - current);
+
+let profit = 1000;
+let sales =500;
+document.writeln(cash + current);
+
+
+let profit1 = 1000;
+let sales2 =500;
+document.writeln(cash * current);
+
+
+let num1 = 7;
+let num2 =9;
+let num3 =2;
+let sum = num1 + num2 + num3;
+let average = sum /3 ;
+document.writeln( "the average is :" + average);
+
+
+
+let price = 150;
+let discount =0.3;
+let multi = price * discount;
+document.writeln( 150 - multi );
+
+let age = 20;
+console.log(age > 18 && age < 30);
+
+
+
+
+console.log(typeof 100);
+console.log(typeof 73.9);
+console.log(typeof NaN);
+console.log(typeof "water");
+console.log(typeof false);
+console.log(typeof 9 != 11);
+console.log(typeof "Orang" + "e");
+console.log(typeof "Orange" - "s");
+console.log(typeof "4" + "8");
+console.log(typeof"4" - "8");
+console.log(typeof  "name" + 3 );
+console.log(typeof  "name" - 3);
+console.log(typeof  82 * "word" );
+console.log(typeof  1 + "hello");
+console.log(typeof  "hello" + 1);
+console.log(typeof  1 + true);
+console.log(typeof  "hello" + true);
+console.log(typeof  typeof (Infinity));
+console.log(typeof  1 == '1');
+console.log(typeof  1 === '1');
